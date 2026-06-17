c6b9247d7e0b7844d7e04b1e6d5913cff0eb51d2
Peter Houwen <peter.houwen@icloud.com>
2026-06-17 22:21:07 +0200

    Refactor GPIO

--- main/src/button.c
+++ main/src/button.c
@@ -6,6 +6,7 @@
  * Includes
  ****************************************************************************/
 #include <stdio.h>
+#include <inttypes.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "esp_log.h"
@@ -13,28 +14,31 @@
 #include "driver/gpio.h"
 #include "io_hal.h"
 
-#define INPUT_PIN 15
+/**
+ * Brief:
+ * 
+ * GPIO status:
+ * GPIO_INPUT_IO_0:  input, pulled up, interrupt from falling edge
+ * 
+ */
+
+#define GPIO_INPUT_IO_0    CONFIG_GPIO_INPUT_0
+#define GPIO_INPUT_PIN_SEL (1ULL<<GPIO_INPUT_IO_0)
+
+#define ESP_INTR_FLAG_DEFAULT 0
 
 static const char *TAG = "button";
 TaskHandle_t xHandleButton = NULL;
 extern TaskHandle_t xHandleController;
 
-
-
 /****************************************************************************
  * Function : gpio_interrupt_handler()
  ****************************************************************************/
-static void gpio_isr_handler( void *arg )
+static void IRAM_ATTR gpio_isr_handler( void *arg )
 {
-//    BaseType_t xHigherPriorityTaskWoken;
-
-//    xHigherPriorityTaskWoken = pdFALSE;
-
     ESP_EARLY_LOGI(TAG, "Button pressed");
     vTaskNotifyGiveFromISR( xHandleController,
                             NULL);
-//    portYIELD_FROM_ISR( xHigherPriorityTaskWoken );
-//    ESP_LOGI(TAG, "Woken %d", xHigherPriorityTaskWoken);
 }
 
 /****************************************************************************
@@ -45,23 +49,32 @@ void button(void *arg)
     uint8_t value = 0;
     hal_io_t io_handle;
 
-    ESP_LOGI(TAG, "button started");
     get_io_handle(&io_handle);
-    
-    gpio_set_direction(INPUT_PIN, GPIO_MODE_INPUT);
-    gpio_set_pull_mode(INPUT_PIN, GPIO_PULLUP_ONLY);
-    gpio_set_intr_type(INPUT_PIN, GPIO_INTR_NEGEDGE);
 
-    gpio_install_isr_service(0);
-    gpio_isr_handler_add(INPUT_PIN, gpio_isr_handler, (void *)INPUT_PIN);
+    gpio_config_t io_conf = {};
+    //interrupt of falling edge
+    io_conf.intr_type = GPIO_INTR_NEGEDGE;
+    //bit mask of the pins
+    io_conf.pin_bit_mask = GPIO_INPUT_PIN_SEL;
+    //set as input mode
+    io_conf.mode = GPIO_MODE_INPUT;
+    //enable pull-up mode
+    io_conf.pull_up_en = GPIO_PULLUP_ENABLE;
+    gpio_config(&io_conf);
+
+    //install gpio isr service
+    gpio_install_isr_service(ESP_INTR_FLAG_DEFAULT);
+    //hook isr handler for specific gpio pin
+    gpio_isr_handler_add(GPIO_INPUT_IO_0, gpio_isr_handler, (void *)GPIO_INPUT_IO_0);
+    ESP_LOGI(TAG, "button started");
     
     while (true)
     {
+        //wait for crosswalk to finish
         ulTaskNotifyTake( pdTRUE, portMAX_DELAY);
-        /* Clear interrupt by reading GPIO */
+        //clear interrupt by reading GPIO
         io_handle.register_read( 0x13, &value, 1);
         ESP_LOGI(TAG, "button is hot");
-        vTaskDelay(100 / portTICK_PERIOD_MS);
     }
 }
 
